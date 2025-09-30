bf1f231806fa8698f32cc6b1d0e13bb40afc9c4c
Gulshan Kumar <gulshan.nitdelhi@gmail.com>
2025-09-30 23:37:26 +0530

    Update dashboard.py
    changes the oschdir line

--- dashboard.py
+++ dashboard.py
@@ -16,7 +16,6 @@ if fl is not None:
     st.write(filename)
     df = pd.read_csv(filename,encoding="ISO-8859-1")
 else:
-    os.chdir(r"A:\streamlit_projects\dashboard project")
     df = pd.read_csv("Superstore.csv",encoding="ISO-8859-1")
 
 col1 , col2 = st.columns((2))
